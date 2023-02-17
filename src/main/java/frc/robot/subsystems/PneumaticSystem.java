682f271a1251be974c46490c0ae674d558d12dd9
Kbratland <kbratland25@riverdale.k12.or.us>
2023-02-16 19:51:33 -0800

    r

--- src/main/java/frc/robot/subsystems/PneumaticSystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticSystem.java
@@ -1,15 +1,29 @@
+
 package frc.robot.subsystems;
 
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticSystem extends SubsystemBase {
-
+    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
     Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
-    Solenoid out = new Solenoid(PneumaticsModuleType.REVPH, 1);
-    Solenoid in = new Solenoid(PneumaticsModuleType.REVPH, 0);
+
+    // pcmCompressor.enableDigital();
+    // pcmCompressor.disable();
+
+    boolean enabled = pcmCompressor.enabled();
+    boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
+    double current = pcmCompressor.getCurrent();
+
+    // DoubleSolenoid exampleDoublePCM = new
+    // DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+    DoubleSolenoid exampleDoublePH = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, 4, 5);
+
+    // exampleDoublePH.set(k)
 
     public void suck() {
         phCompressor.enableDigital();
@@ -20,13 +34,10 @@ public class PneumaticSystem extends SubsystemBase {
     }
 
     public void reverse() {
-        out.set(true);
+        // out.set(true);
     }
 
     public void thrust() {
-        in.set(true);
+        // in.set(true);
     }
-
-    // boolean enabled = pcmCompressor.enabled();
-    // boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
 }
