546aa0db248bf5a169d4739399c83d2a555a8bcc
Kbratland <kbratland25@riverdale.k12.or.us>
2023-02-16 18:48:12 -0800

    help arm broke

---
+++ src/main/java/frc/robot/commands/armDown.java
@@ -0,0 +1,31 @@
+// package frc.robot.commands;
+
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.Armticulation;
+
+// public class armDown extends CommandBase {
+// public Armticulation armticulation;
+
+// public armDown(Armticulation armticulation) {
+// addRequirements(armticulation);
+// this.armticulation = armticulation;
+// }
+
+// @Override
+// public void initialize() {
+
+// }
+
+// public void execute() {
+// armticulation.lift();
+// }
+
+// public boolean isFinished() {
+// return false;
+// }
+
+// public void end(boolean interrupted) {
+// armticulation.brokenArm();
+// }
+
+// }
